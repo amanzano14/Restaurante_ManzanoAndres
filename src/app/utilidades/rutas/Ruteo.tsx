c2b5aa4d2be9805ce502d869806ae1b4b69180d9
amanzano1 <amanzano1@cuc.edu.co>
2024-10-17 12:05:34 -0500

    hasta inicio

--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -12,13 +12,16 @@ export const Ruteo = () =>{
         <Routes>
             <Route>
                 <Route path="/" element= {<Inicio />} />
+
                 <Route path="/mencre" element= {<MenuCrear />} />
                 <Route path="/menlis" element= {<MenuListar />} />
                 <Route path="/menadmin" element= {<MenuAdmin />} />
-
                 <Route path="/menactu/: codigo" element= {<MenuActualizar />} />
+
                 <Route path="/menuacer" element= {<Acerca />} />
+
                 <Route path="*" element= {<NoEncontrado />} />
+                
             </Route>
         </Routes>
     )

02d7d72dc441060903f98ffb6c2e0c8169417afc
amanzano1 <amanzano1@cuc.edu.co>
2024-11-21 10:17:03 -0500

    CRUD Menu Listar

--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Inicio } from "../../componentes/Inicio";
+import { Inicio } from "../../componentes/contenedor/Inicio";
 import { MenuCrear } from "../../componentes/menu/MenuCrear";
 import { MenuListar } from "../../componentes/menu/MenuListar";
 import { MenuAdmin } from "../../componentes/menu/MenuAdmin";
 import { MenuActualizar } from "../../componentes/menu/MenuActualizar";
 import { Acerca } from "../../componentes/otros/Acerca";
-import { NoEncontrado } from "../../componentes/NoEncontrado";
+import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado";
 
 export const Ruteo = () => {
   return (

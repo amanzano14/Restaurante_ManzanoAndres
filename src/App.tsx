f94ebdeb1f8cc19223b661cc6605f948a62f6ace
amanzano1 <amanzano1@cuc.edu.co>
2024-11-21 10:20:02 -0500

    CRUD Menu Listar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { Cabecera } from "./app/componentes/Cabecera";
+import { Cabecera } from "./app/componentes/contenedor/Cabecera";
 import { Ruteo } from "./app/utilidades/rutas/Ruteo";
 
 function App() {
